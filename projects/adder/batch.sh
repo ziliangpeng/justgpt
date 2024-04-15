fc0e327aa755e4d1705935e57b92afd6f04a37a4
Ziliang Peng <ziliangdotme@gmail.com>
2024-04-14 20:52:03 -0700

    add a batch job to run several experiments

---
+++ projects/adder/batch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+python longadder.py --trainer.device=mps --data.ndigit=1
+python longadder.py --trainer.device=mps --data.ndigit=1
+python longadder.py --trainer.device=mps --data.ndigit=1
+python longadder.py --trainer.device=mps --data.ndigit=2
+python longadder.py --trainer.device=mps --data.ndigit=2
+python longadder.py --trainer.device=mps --data.ndigit=2
+python longadder.py --trainer.device=mps --data.ndigit=3
+python longadder.py --trainer.device=mps --data.ndigit=3
+python longadder.py --trainer.device=mps --data.ndigit=3
+python longadder.py --trainer.device=mps --data.ndigit=4
+python longadder.py --trainer.device=mps --data.ndigit=4
+python longadder.py --trainer.device=mps --data.ndigit=4
+python longadder.py --trainer.device=mps --data.ndigit=5
+python longadder.py --trainer.device=mps --data.ndigit=5
+python longadder.py --trainer.device=mps --data.ndigit=5
+python longadder.py --trainer.device=mps --data.ndigit=6
+python longadder.py --trainer.device=mps --data.ndigit=6
+python longadder.py --trainer.device=mps --data.ndigit=6
+python longadder.py --trainer.device=mps --data.ndigit=6 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=6 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=6 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=7
+python longadder.py --trainer.device=mps --data.ndigit=7
+python longadder.py --trainer.device=mps --data.ndigit=7
+python longadder.py --trainer.device=mps --data.ndigit=7 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=7 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=7 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=8
+python longadder.py --trainer.device=mps --data.ndigit=8
+python longadder.py --trainer.device=mps --data.ndigit=8
+python longadder.py --trainer.device=mps --data.ndigit=8 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=8 --model.model_type=gpt-micro
+python longadder.py --trainer.device=mps --data.ndigit=8 --model.model_type=gpt-micro
